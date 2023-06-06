c9b7cbb5c47d8dc9a317dfe6c031deee2e1a9c44
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-06-06 10:58:01 +0800

    Bench 6414758
    further minify

--- src/main.cpp
+++ src/main.cpp
@@ -833,32 +833,32 @@ Move iteratively_deepen(Position &pos,
         
         i32 window = 32 + score * score / 16384;
         while (true) {
-            const i32 newscore = alphabeta(pos,
-                                            score - window,
-                                            score + window,
-                                            i,
-                                            0,
-                                            // minify enable filter delete
-                                            nodes,
-                                            // minify disable filter delete
-                                            start_time + allocated_time,
-                                            stop,
-                                            stack,
-                                            hh_table,
-                                            hash_history);
+            score = alphabeta(pos,
+                              alpha,
+                              beta,
+                              i,
+                              0,
+                              // minify enable filter delete
+                              nodes,
+                              // minify disable filter delete
+                              start_time + allocated_time,
+                              stop,
+                              stack,
+                              hh_table,
+                              hash_history);
                 
             // minify enable filter delete
             // The main search thread prints with every iteration normally, or when the target depth has finished when
             // benchmarking
             if (thread_id == 0 &&
-                (bench_depth == 0 || i == bench_depth && newscore < score + window && newscore > score - window)) {
+                (bench_depth == 0 || i == bench_depth && score < beta && score > alpha)) {
                 const u64 elapsed = now() - start_time;
                 cout << "info";
                 cout << " depth " << i;
-                cout << " score cp " << newscore;
-                if (newscore >= score + window) {
+                cout << " score cp " << score;
+                if (score >= beta) {
                     cout << " lowerbound";
-                } else if (newscore <= score - window) {
+                } else if (score <= alpha) {
                     cout << " upperbound";
                 }
                 cout << " time " << elapsed;
@@ -867,7 +867,7 @@ Move iteratively_deepen(Position &pos,
                     cout << " nps " << nodes * 1000 / elapsed;
                 }
                 // Not a lowerbound - a fail low won't have a meaningful PV.
-                if (newscore > score - window) {
+                if (score > alpha) {
                     cout << " pv";
                     print_pv(pos, stack[0].move, hash_history);
                 }
@@ -875,13 +875,12 @@ Move iteratively_deepen(Position &pos,
             }
 
             // OpenBench compliance
-            if (bench_depth > 0 && i >= bench_depth && newscore < score + window && newscore > score - window) {
+            if (bench_depth > 0 && i >= bench_depth && score < beta && score > alpha) {
                 total_nodes += nodes;
                 return no_move;
             }
             // minify disable filter delete
 
-            score = newscore;
             if (score <= alpha) {
 
                 beta = (alpha + beta) / 2;
