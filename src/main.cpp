518a57b6cdf3a36ef5baa69f1719f8027692e3a2
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-06-14 17:40:06 +0800

    Bench 5402072
    Credit goes to Shaheryar (StockDory author)

--- src/main.cpp
+++ src/main.cpp
@@ -580,7 +580,7 @@ i32 alphabeta(Position &pos,
                 if (-alphabeta(npos,
                                -beta,
                                -beta + 1,
-                               depth - 4 - depth / 6 - min((static_eval - beta) / 200, 3),
+                               depth - 4 - depth / 6 - min((static_eval - beta) / 200, 3) - improving,
                                ply + 1,
                                // minify enable filter delete
                                nodes,
