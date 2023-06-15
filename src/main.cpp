99ed44a4549e9dc0c50b12664f6cb086aab33af6
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-06-15 21:09:31 +0800

    Bench 6692678

--- src/main.cpp
+++ src/main.cpp
@@ -573,7 +573,7 @@ i32 alphabeta(Position &pos,
             }
 
             // Null move pruning
-            if (depth > 2 && static_eval >= beta && do_null && pos.colour[0] & ~(pos.pieces[Pawn] | pos.pieces[King])) {
+            if (depth > 2 && static_eval >= beta - 64 * improving && do_null && pos.colour[0] & ~(pos.pieces[Pawn] | pos.pieces[King])) {
                 Position npos = pos;
                 flip(npos);
                 npos.ep = 0;
