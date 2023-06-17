63d215a635fe5e8762eafc1170efbecf7cd6b397
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-06-17 18:42:34 +0800

    Tweak

--- src/main.cpp
+++ src/main.cpp
@@ -1118,7 +1118,7 @@ i32 main(
             // minify disable filter delete
 
             const u64 start = now();
-            const u64 allocated_time = ((pos.flipped ? btime : wtime) - 5) / 10;
+            const u64 allocated_time = ((pos.flipped ? btime : wtime) - 50) / 20;
 
             // Lazy SMP
             vector<thread> threads;
