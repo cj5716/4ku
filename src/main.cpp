1426a8b2e6327c7f9c1a1b71e7e52546766e73c0
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-06-17 13:30:52 +0800

    Bench 5885552

--- src/main.cpp
+++ src/main.cpp
@@ -532,7 +532,7 @@ i32 alphabeta(Position &pos,
     const i32 in_qsearch = depth <= 0;
     const u64 tt_key = get_hash(pos);
 
-    if (ply > 0 && !in_qsearch) {
+    if (ply > 0 || in_qsearch) {
         // Repetition detection
         for (const u64 old_hash : hash_history)
             if (old_hash == tt_key)
