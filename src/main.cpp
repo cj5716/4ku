94c2afba96841f44e432247f5e6cc0456129fdea
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-06-19 20:27:45 +0800

    Bench 5662189

--- src/main.cpp
+++ src/main.cpp
@@ -664,7 +664,8 @@ i32 alphabeta(Position &pos,
         // minify disable filter delete
 
         i32 score;
-        i32 reduction = (alpha == beta - 1) + !improving - static_cast<i32>(hh_table[pos.flipped][move.from][move.to] / 8192);
+        i32 reduction = 1 + num_moves_evaluated / 14 + depth / 17 + (alpha == beta - 1) - improving -
+                                      max(-1L, min(hh_table[pos.flipped][move.from][move.to], 1L));
         
         if (depth > 2 && num_moves_evaluated > 4 && !gain) {
             reduction = max(-1, min(reduction, depth - 2));
