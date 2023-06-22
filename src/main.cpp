9fadb94b814f3973a4783b714604dcab4e292e4f
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-06-22 10:13:09 +0800

    Bench 9448206

--- src/main.cpp
+++ src/main.cpp
@@ -682,7 +682,7 @@ i32 alphabeta(Position &pos,
         } else {
             // Late move reduction
             i32 reduction = depth > 2 && num_moves_evaluated > 4 && !gain
-                                ? 1 + num_moves_evaluated / 14 + depth / 17 + (alpha == beta - 1) + !improving -
+                                ? num_moves_evaluated / 14 + depth / 17 + (alpha == beta - 1) + !improving -
                                   min(max(hh_table[pos.flipped][move.from][move.to] / 2048, -1L), 1L)
                                 : 0;
             reduction = min(max(reduction, 0), depth - 2);
