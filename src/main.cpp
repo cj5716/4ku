01718ecfc52055736e8b82e1ee29c711946de55f
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-07-01 12:11:51 +0800

    Bench 5224614

--- src/main.cpp
+++ src/main.cpp
@@ -885,7 +885,7 @@ Move iteratively_deepen(Position &pos,
         score = newscore;
 
         // Early exit after completed ply
-        if (!research && now() >= start_time + allocated_time / 7)
+        if (!research && now() >= start_time + allocated_time / 6)
             break;
     }
     return stack[0].move;
@@ -1118,7 +1118,7 @@ i32 main(
             // minify disable filter delete
 
             const u64 start = now();
-            const u64 allocated_time = ((pos.flipped ? btime : wtime) - 50) / 4;
+            const u64 allocated_time = ((pos.flipped ? btime : wtime) - 50) / 5;
 
             // Lazy SMP
             vector<thread> threads;
