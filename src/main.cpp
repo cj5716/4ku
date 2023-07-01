263e7ec30ee329e07f5b3d938fae2a87cdd07abc
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-07-01 16:06:04 +0800

    Bench 7946688

--- src/main.cpp
+++ src/main.cpp
@@ -686,6 +686,7 @@ i32 alphabeta(Position &pos,
                                       (hh_table[pos.flipped][move.from][move.to] < 0) -
                                       (hh_table[pos.flipped][move.from][move.to] > 0)
                                 : 0;
+            reduction = min(reduction, depth - 2);
 
         zero_window:
             score = -alphabeta(npos,
