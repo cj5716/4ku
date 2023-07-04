7387cfe56e58f54545edb552a9aa5dbdfd98f0b8
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-07-04 15:51:08 +0800

    Bench 5273688

--- src/main.cpp
+++ src/main.cpp
@@ -681,10 +681,11 @@ i32 alphabeta(Position &pos,
                                hash_history);
         } else {
             // Late move reduction
-            i32 reduction = depth > 2 && num_moves_evaluated > 4 && !gain
-                                ? 1 + num_moves_evaluated / 14 + depth / 17 + (alpha == beta - 1) - improving +
-                                      (hh_table[pos.flipped][move.from][move.to] < 0) -
-                                      (hh_table[pos.flipped][move.from][move.to] > 0)
+            i32 reduction = depth > 2 && num_moves_evaluated > 4
+                                ? !gain ? 1 + num_moves_evaluated / 14 + depth / 17 + (alpha == beta - 1) - improving +
+                                              (hh_table[pos.flipped][move.from][move.to] < 0) -
+                                              (hh_table[pos.flipped][move.from][move.to] > 0)
+                                        : 1
                                 : 0;
 
         zero_window:
