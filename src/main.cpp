3063fdc3daa4381027f5cba358b64142a282f7cf
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-07-04 15:44:34 +0800

    Bench 5224614

--- src/main.cpp
+++ src/main.cpp
@@ -681,12 +681,11 @@ i32 alphabeta(Position &pos,
                                hash_history);
         } else {
             // Late move reduction
+            i32 hist = hh_table[pos.flipped][move.from][move.to];
             i32 reduction = depth > 2 && num_moves_evaluated > 4 && !gain
-                                ? 1 + num_moves_evaluated / 14 + depth / 17 + (alpha == beta - 1) - improving +
-                                      (hh_table[pos.flipped][move.from][move.to] < 0) -
-                                      (hh_table[pos.flipped][move.from][move.to] > 0)
+                                ? num_moves_evaluated / 14 + depth / 17 + (alpha == beta - 1) + !improving -
+                                  min(max(hist, -1), 1) + (hist <= -1536)
                                 : 0;
-
         zero_window:
             score = -alphabeta(npos,
                                -alpha - 1,
