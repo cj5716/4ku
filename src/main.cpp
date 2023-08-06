2cf7f08a20c9bc0d97b39c0435001fb57cf57b71
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-08-07 07:35:36 +0800

    Bench 5358264

--- src/main.cpp
+++ src/main.cpp
@@ -645,12 +645,6 @@ i32 alphabeta(Position &pos,
         // Material gain
         const i32 gain = max_material[move.promo] + max_material[piece_on(pos, move.to)];
 
-        // Delta pruning
-        if (in_qsearch && !in_check && static_eval + 50 + gain < alpha) {
-            best_score = alpha;
-            break;
-        }
-
         // Forward futility pruning
         if (depth < 8 && !in_qsearch && !in_check && !(move == tt_move) && static_eval + 100 * depth + gain < alpha) {
             best_score = alpha;
