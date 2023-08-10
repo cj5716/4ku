6b28d1aa1f2d46373bca1970ec7172a652270a9c
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-08-10 10:41:03 +0800

    Bench 5433785

--- src/main.cpp
+++ src/main.cpp
@@ -580,21 +580,22 @@ i32 alphabeta(Position &pos,
             Position npos = pos;
             flip(npos);
             npos.ep = 0;
-            if (-alphabeta(npos,
-                           -beta,
-                           -beta + 1,
-                           depth - 4 - depth / 6 - min((static_eval - beta) / 200, 3),
-                           ply + 1,
-                           // minify enable filter delete
-                           nodes,
-                           // minify disable filter delete
-                           stop_time,
-                           stop,
-                           stack,
-                           hh_table,
-                           hash_history,
-                           false) >= beta)
-                return beta;
+            i32 score = -alphabeta(npos,
+                                   -beta,
+                                   -beta + 1,
+                                   depth - 4 - depth / 6 - min((static_eval - beta) / 200, 3),
+                                   ply + 1,
+                                   // minify enable filter delete
+                                   nodes,
+                                   // minify disable filter delete
+                                   stop_time,
+                                   stop,
+                                   stack,
+                                   hh_table,
+                                   hash_history,
+                                   false);
+            if (score >= beta)
+                return score > mate_score - 128 ? beta : score;
         }
     }
 
