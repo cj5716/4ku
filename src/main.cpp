1797ebba623fe0ec65231671d086f4b52fced3e7
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-09-09 09:13:48 +0800

    Bench 5454265

--- src/main.cpp
+++ src/main.cpp
@@ -53,7 +53,7 @@ enum
 [[nodiscard]] u64 now() {
     timespec t;
     clock_gettime(CLOCK_MONOTONIC, &t);
-    return t.tv_sec * 1000 + t.tv_nsec / 1000000;
+    return t.tv_sec * 1000;
 }
 
 struct [[nodiscard]] Position {
