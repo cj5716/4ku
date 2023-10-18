e870a7d38b10c589d729f53b3b72e3ff426918d5
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-10-18 08:21:19 +0800

    Bench 5538331

--- src/main.cpp
+++ src/main.cpp
@@ -735,7 +735,7 @@ i32 alphabeta(Position &pos,
                     depth * depth - hh_table[pos.flipped][move.from][move.to] * depth * depth / 16384;
                 for (i32 j = 0; j < num_quiets_evaluated - 1; ++j)
                     hh_table[pos.flipped][stack[ply].quiets_evaluated[j].from][stack[ply].quiets_evaluated[j].to] -=
-                        depth * depth +
+                        depth * depth -
                         hh_table[pos.flipped][stack[ply].quiets_evaluated[j].from][stack[ply].quiets_evaluated[j].to] *
                             depth * depth / 16384;
                 stack[ply].killer = move;
