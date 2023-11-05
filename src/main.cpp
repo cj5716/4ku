240954cace694df7daebee2c4a86b67934233b1d
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-11-05 21:14:40 +0800

    Bench 5211618

--- src/main.cpp
+++ src/main.cpp
@@ -621,7 +621,7 @@ i32 alphabeta(Position &pos,
             if (-alphabeta(npos,
                            -beta,
                            -alpha,
-                           depth - 4 - depth / 6 - min((static_eval - beta) / 200, 3),
+                           depth - 3 - depth / 3 - min((static_eval - beta) / 200, 3),
                            ply + 1,
                            // minify enable filter delete
                            nodes,
