824a110ae979976b0e2075964e5c7d0ccfbfcbeb
cj5716 <125858804+cj5716@users.noreply.github.com>
2023-11-23 08:06:59 +0800

    Bench 4661713

--- src/main.cpp
+++ src/main.cpp
@@ -466,7 +466,7 @@ const i32 pawn_passed_blocked_penalty[] = {S(9, 14), S(-7, 43), S(-9, 85), S(4, 
 const i32 pawn_passed_king_distance[] = {S(1, -6), S(-4, 11)};
 const i32 bishop_pair = S(32, 72);
 const i32 king_shield[] = {S(36, -12), S(27, -7)};
-const i32 pawn_attacked_penalty[] = {S(63, 14), S(156, 140)};
+const i32 pawn_attacked_penalty[] = {S(60, 14), S(155, 140)};
 
 [[nodiscard]] i32 eval(Position &pos) {
     // Include side to move bonus
@@ -690,12 +690,12 @@ i32 alphabeta(Position &pos,
 
     if (ply > 0 && !in_qsearch && !in_check && alpha == beta - 1) {
         // Reverse futility pruning
-        if (depth < 8) {
+        if (depth < 9) {
             assert(ply > 0);
-            if (static_eval - 68 * (depth - improving) >= beta)
+            if (static_eval - 70 * (depth - improving) >= beta)
                 return static_eval;
 
-            in_qsearch = static_eval + 256 * depth < alpha;
+            in_qsearch = static_eval + 232 * depth < alpha;
         }
 
         // Null move pruning
@@ -708,7 +708,7 @@ i32 alphabeta(Position &pos,
             if (-alphabeta(npos,
                            -beta,
                            -alpha,
-                           depth - 4 - depth / 5 - min((static_eval - beta) / 200, 3),
+                           depth - 4 - depth / 4 - min((static_eval - beta) / 195, 3),
                            ply + 1,
                            // minify enable filter delete
                            nodes,
@@ -766,12 +766,12 @@ i32 alphabeta(Position &pos,
         const i32 gain = max_material[move.promo] + max_material[piece_on(pos, move.to)];
 
         // Delta pruning
-        if (in_qsearch && !in_check && static_eval + 48 + gain < alpha)
+        if (in_qsearch && !in_check && static_eval + 49 + gain < alpha)
             break;
 
         // Forward futility pruning
         if (ply > 0 && depth < 8 && !in_qsearch && !in_check && num_moves_evaluated &&
-            static_eval + 104 * depth + gain < alpha)
+            static_eval + 102 * depth + gain < alpha)
             break;
 
         Position npos = pos;
@@ -801,7 +801,7 @@ i32 alphabeta(Position &pos,
         else {
             // Late move reduction
             i32 reduction = depth > 2 && num_moves_evaluated > 4 && !gain
-                                ? num_moves_evaluated / 13 + depth / 15 + (alpha == beta - 1) + !improving +
+                                ? num_moves_evaluated / 12 + depth / 15 + (alpha == beta - 1) + !improving +
                                       (hh_table[pos.flipped][move.from][move.to] < 0) -
                                       (hh_table[pos.flipped][move.from][move.to] > 0)
                                 : 0;
